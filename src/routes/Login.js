a6a534dfdb62bb27128dd74033bedf654b40b87b
heejintv <144670992+heejintv@users.noreply.github.com>
2023-11-14 19:33:10 +0900

    ,
    ,

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,18 +1,46 @@
-function RegistrationLogin() {
-    return (
-        <form>
-            <h1>SKIN FOR</h1>
-            <div>email : <input type="email"></input></div>
-            <div>password : <input type="password"></input></div>
-            <button type="submit">회원가입 하기</button>
-            <button>로그인 하러 가기</button>
-        
-        </form>
-       
-      );
-}
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export default RegistrationLogin;
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.email);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
 
 
 
