3609915e60deac2e21ba2dbb63168278f550cabe
gyunho <124599614+g0rnn@users.noreply.github.com>
2023-11-21 00:15:11 +0900

    feat(update Navbars): 버튼을 누르면 해당 페이지로 이동

--- src/components/Navbars2.js
+++ src/components/Navbars2.js
@@ -1,14 +1,32 @@
 import "../css/Navbar2.css";
+import { useNavigate } from "react-router-dom";
 
 function Navbars() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <header className="navbar-style-7">
         <img className="logo_img" src="./images/logo_black.png" alt="LOGO" />
         <div className="nav_list">
-          <button className="nav_button">Cosmetics</button>
-          <button className="nav_button">Community</button>
-          <button className="nav_button">Mypage</button>
+          <button
+            className="nav_button"
+            onClick={() => navigate(`${process.env.PUBLIC_URL}/main`)}
+          >
+            Cosmetics
+          </button>
+          <button
+            className="nav_button"
+            onClick={() => navigate(`${process.env.PUBLIC_URL}/community`)}
+          >
+            Community
+          </button>
+          <button
+            className="nav_button"
+            onClick={() => navigate(`${process.env.PUBLIC_URL}/mypage`)}
+          >
+            Mypage
+          </button>
         </div>
       </header>
     </div>
