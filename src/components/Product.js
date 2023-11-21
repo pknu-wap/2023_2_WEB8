dc7daf1946ce582b19b0e806e12e31b8c7f98367
heejintv <144670992+heejintv@users.noreply.github.com>
2023-11-21 20:35:35 +0900

    제품 css
    1차 완성

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,20 @@
+import React from "react";
 import LabelBtn from "./LabelBtn";
+import "../css/Product.css"; // Import the CSS file
 
 function Product({ id, name, price, rank, uid }) {
   return (
-    <div>
-      <div>{name}</div>
-      <div>{price}</div>
-      <div>{rank}</div>
+    <div className="product-container">
+      <div className="product-image">
+        {/* <img src={imageUrl} alt={name} /> */}
+        <img src="./images/community.png" alt="LOGO" />
+      </div>
+      <div className="product-info">
+        <div className="product-name">{name}</div>
+        <div className="product-price">{price}</div>
+        <div className="product-rank">{rank}</div>
+      </div>
+
       <LabelBtn productName={name} userId={uid} />
     </div>
   );
