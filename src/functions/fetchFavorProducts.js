0c26e3ed9056746c9da1c6993e3c4e176a881063
gyunho <124599614+g0rnn@users.noreply.github.com>
2023-11-29 23:27:08 +0900

    bugFix: 마이페이지 즐겨찾는 제품 사진추가, 스크랩 실시간으로 확인 가능

--- src/functions/fetchFavorProducts.js
+++ src/functions/fetchFavorProducts.js
@@ -7,11 +7,13 @@ import {
   query,
   where,
 } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const fetchFavorProducts = async (setProducts, userid) => {
   try {
     const docRef = doc(db, "Users", userid);
     const userSnapshot = await getDoc(docRef);
+    const storage = getStorage();
 
     if (userSnapshot.exists()) {
       const favorites = userSnapshot.data().favorites || [];
@@ -27,13 +29,33 @@ const fetchFavorProducts = async (setProducts, userid) => {
       );
 
       const querySnapshot = await getDocs(productQuery);
-      const favorProducts = [];
+      const productPromises = querySnapshot.docs.map(async (doc) => {
+        const product = doc.data();
+        let imageURL = "";
 
-      querySnapshot.forEach((doc) => {
-        favorProducts.push(doc.data());
+        try {
+          imageURL = await getDownloadURL(
+            ref(storage, `products_image/${product.Name + ".jpeg"}`)
+          );
+        } catch (error) {
+          console.log("No img in product >>> ", error);
+          imageURL = "/images/5.jpg";
+        }
+
+        return {
+          id: doc.id,
+          Brand: product.Brand,
+          Name: product.Name,
+          Label: product.Label,
+          Rank: product.Rank,
+          Price: product.Price,
+          Ingredients: product.Ingredients,
+          imgURL: imageURL,
+        };
       });
 
-      setProducts(favorProducts);
+      const productData = await Promise.all(productPromises);
+      setProducts(productData);
     }
   } catch (error) {
     console.error("Error in fetchFavorProducts <<< ", error);
